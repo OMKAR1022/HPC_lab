bc202e53c4917a3585809b4beef481c49b09bdd4
Omkar Oholomkar@22 <oholomkar40@gmail.com>
2024-08-01 13:42:55 +0530

    Delete Assignment_2 directory

--- Assignment_2/11.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    // argc contains the number of command-line arguments
-    printf("Number of command-line arguments: %d\n", argc);
-
-    return 0;
-}
--- Assignment_2/13.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    // Print the number of command-line arguments
-    printf("Number of command-line arguments: %d\n", argc - 1);
-
-    return 0;
-}
